bfcc4dbd49e056c85b5a07cc71588d8df617fb0d
Saad Mountain <saadmoun@ge-b3.kzn.21-school.ru>
2024-12-28 16:45:09 +0300

    develop mod .h

--- src/s21_string.h
+++ src/s21_string.h
@@ -20,15 +20,20 @@ typedef struct {
   int size;
 } Settings;
 
-void *s21_memchr(const void *str, int c, s21_size_t n);
+void* s21_memchr(const void *str, int c, s21_size_t n);
 int s21_memcmp(const void *str1, const void *str2, s21_size_t n);
-void *s21_memcpy(void *dest, const void *src, s21_size_t n);
-void *s21_memset(void *str, int c, s21_size_t n);
+void* s21_memcpy(void *dest, const void *src, s21_size_t n);
+void* s21_memset(void *str, int c, s21_size_t n);
 char *s21_strncat(char *dest, const char *src, s21_size_t n);
 char *s21_strchr(const char *str, int c);
 int s21_strncmp(const char *str1, const char *str2, s21_size_t n);
 char *s21_strncpy(char *dest, const char *src, s21_size_t n);
 s21_size_t s21_strcspn(const char *str1, const char *str2);
 s21_size_t s21_strlen(const char *src);
+char *s21_strerror(int errnum);
+char *s21_strpbrk(const char *str1, const char *str2);
+char *s21_strrchr(const char *str, int symbol);
+char *s21_strstr(const char *haystack, const char *needle);
+char *s21_strtok(char *str, const char *delim);
 
 #endif
